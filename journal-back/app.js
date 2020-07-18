5c7404912df0644a70cd5cf4d28a1c2c781da93b
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-07-18 19:58:09 +0200

    created user model and route

--- journal-back/app.js
+++ journal-back/app.js
@@ -1,11 +1,22 @@
 var createError = require('http-errors');
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/journalapp'
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},(err) => {
+    if(!err) return console.log("succesfully connected to db")
+    console.log(err)
+})
 
 var app = express();
 
@@ -15,13 +26,13 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -30,7 +41,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.article = err.article;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
@@ -39,3 +50,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- journal-back/routes/users.js
+++ journal-back/routes/users.js
@@ -1,9 +1,105 @@
 var express = require('express');
-var router = express.Router();
+var users = express.Router();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
+const User = require("../models/User");
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+  const userData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
+  const { name, email, password } = req.body
+  if (name == "" || email == "" || password == "") {
+    res.json({ error: 'All fields are required' })
+  }
+  else {
+    User.findOne({
+      email: req.body.email
+    })
+      .then(user => {
+        if (!user) {
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            userData.password = hash
+            User.create(userData)
+              .then(user => {
+                res.json({ status: user.name + ' registered!' })
+              })
+              .catch(err => {
+                res.send('error: ' + err)
+              })
+          })
+        } else {
+          res.json({ error: ' user already exists' })
+        }
+      })
+      .catch(err => {
+        res.send('error: ' + err)
+      })
+  }
+})
+
+users.post('/login', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          const payload = {
+            _id: user._id,
+            name: user.name,
+            email: user.email
+          }
+          let token = jwt.sign(payload, process.env.SECRET_KEY, {
+            expiresIn: 1440
+          })
+          res.json(payload)
+        } else {
+          res.json({ error: "Incorrect Password" })
+        }
+      } else {
+        res.json({ error: "User does not exist" })
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+users.get('/', function (req, res) {
+  User.find({}, function(err, users) {
+    var userMap = {};
+
+    users.forEach(function(user) {
+      userMap[user._id] = user;
+    });
+
+    res.send(userMap);  
+  });
+});
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+users.get('/profile', (req, res) => {
+  var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+
+  User.findOne({
+    _id: decoded._id
+  })
+    .then(user => {
+      if (user) {
+        res.json(user)
+      } else {
+        res.send("User does not exist profile")
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
 });
 
-module.exports = router;
+module.exports = users;
