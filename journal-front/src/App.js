4ff283cbcc9a1377d4e68c0777c3032fe7eb86a9
Lamis Elzobeidy <lamis.zobeidy@gmail.com>
2020-07-18 20:03:06 +0200

    Registeration and Login front end

--- journal-front/src/App.js
+++ journal-front/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Registeration from './components/registeration/Registeration';
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" exact component={Registeration} />
+        <Route path="/login" component={Login} />
+        </Switch>
     </div>
   );
 }
